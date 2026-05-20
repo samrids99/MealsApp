51c2aca1c60e6fc4f2dff70c024d9a4e11e41ca4
Sam Ridsdale <samridsdale108@gmail.com>
2026-05-20 19:49:24 +0100

    outputting content in meal details screen

--- components/MealItem.js
+++ components/MealItem.js
@@ -6,6 +6,7 @@ import {
   StyleSheet,
   Platform,
 } from "react-native";
+import MealDetails from "./MealDetails";
 
 function MealItem({
   title,
@@ -26,11 +27,11 @@ function MealItem({
             <Image source={{ uri: imageUrl }} style={styles.image} />
             <Text style={styles.title}>{title}</Text>
           </View>
-          <View style={styles.details}>
-            <Text style={styles.detailItem}>{duration}m</Text>
-            <Text style={styles.detailItem}>{complexity.toUpperCase()}</Text>
-            <Text style={styles.detailItem}>{affordability.toUpperCase()}</Text>
-          </View>
+          <MealDetails
+            complexity={complexity}
+            affordability={affordability}
+            duration={duration}
+          />
         </View>
       </Pressable>
     </View>
@@ -64,16 +65,6 @@ const styles = StyleSheet.create({
     textAlign: "center",
     fontSize: 18,
     margin: 8,
-  },
-  details: {
-    flexDirection: "row",
-    alignItems: "center",
-    padding: 8,
-    justifyContent: "center",
-  },
-  detailItem: {
-    marginHorizontal: 4,
-    fontSize: 12,
   },
   buttonPressed: {
     opacity: 0.5,
--- screens/MealsDetailsScreen.js
+++ screens/MealsDetailsScreen.js
@@ -1,6 +1,7 @@
-import { Text } from "react-native";
+import { Image, Text, StyleSheet, View } from "react-native";
 import { useLayoutEffect } from "react";
 import { MEALS } from "../data/dummy-data";
+import MealDetails from "../components/MealDetails";
 
 function MealsDetailsScreen({ route, navigation }) {
   const mealId = route.params.mealId;
@@ -13,7 +14,33 @@ function MealsDetailsScreen({ route, navigation }) {
     });
   }, [mealId, navigation]);
 
-  return <Text>Hello, world</Text>;
+  const selectedMeal = MEALS.find((meal) => meal.id == mealId);
+
+  return (
+    <>
+      <View>
+        <Image source={{ uri: selectedMeal.imageUrl }} />
+        <Text>{selectedMeal.title}</Text>
+        <View>
+          <MealDetails
+            complexity={selectedMeal.complexity}
+            affordability={selectedMeal.affordability}
+            duration={selectedMeal.duration}
+          />
+        </View>
+        <Text>Ingredients</Text>
+        {selectedMeal.ingredients.map((ingredient) => (
+          <Text key={ingredient}>{ingredient}</Text>
+        ))}
+        <Text>Steps</Text>
+        {selectedMeal.steps.map((step) => (
+          <Text key={step}>{step}</Text>
+        ))}
+      </View>
+    </>
+  );
 }
 
 export default MealsDetailsScreen;
+
+const styles = StyleSheet.create({});
